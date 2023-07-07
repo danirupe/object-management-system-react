0021214f349afb9b1ecdefd760cf993452b5f343
Daniel Ruiz Pérez <danirpsr1@gmail.com>
2023-07-07 15:33:22 +0100

    Updated base URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/object-management-system-react/',
+  base: './',
   plugins: [react()],
   test: {
     environment: 'jsdom',
